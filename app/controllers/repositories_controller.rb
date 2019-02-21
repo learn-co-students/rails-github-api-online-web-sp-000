801b6712b00fb3caadd91a219147caa0f5383371
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-21 18:29:09 -0500

    get correct user body in index

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,12 @@
 class RepositoriesController < ApplicationController
-  
   def index
+    res = Faraday.get('https://api.github.com/user') do |req|
+      req.headers = {
+        'Accept': 'application/json',
+        'Authorization': "token #{session[:token]}"
+      }
+    end
+    @body = JSON.parse(res.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,9 @@ class SessionsController < ApplicationController
         'code': code, 
         'client_id': client_id,
         'client_secret': client_secret }
-      req.headers['Aceept'] = 'application/json'
+      req.headers['Accept'] = 'application/json'
     end
     body = JSON.parse(res.body)
-    binding.pry if !body['access_token']
     session[:token] = body['access_token']
     redirect_to root_path
   end
