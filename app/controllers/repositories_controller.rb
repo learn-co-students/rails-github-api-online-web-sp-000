caf9f1004a3e3de98f2cf6e72301d5cb242544ec
tiffanynuugen <tiffanynuugen@gmail.com>
2019-05-04 00:15:52 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -16,7 +16,7 @@ class RepositoriesController < ApplicationController
 
   def create
     resp = Faraday.post "https://api.github.com/user/repos" do |req|
-      req.body = { name: params['name'] }.to_json
+      req.body = { name: params[:name] }.to_json
       req.headers['Authorization'] = "token #{session[:token]}"
     end
     body = JSON.parse(resp.body)
