f8ebc0942b1015f1aca82d440507628870594330
blainelawson <blaine.lawson@gmail.com>
2019-08-01 18:55:24 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,25 @@
 class RepositoriesController < ApplicationController
   
   def index
-  end
+    client_id = ENV['GITHUB_CLIENT']
+    client_secret = ENV['GITHUB_SECRET']
 
+    response = Faraday.get "https://api.github.com/user" do |req|
+      req.params = { 'access_token': session[:token] }
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(response.body)
+     
+    @user = body["login"]
+
+    response = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.params = { 'access_token': session[:token] }
+      req.headers['Accept'] = 'application/json'
+    end
+    
+    @repos = JSON.parse(response.body)
+
+    # binding.pry
+  end
 end
