bdde20b07de7e49b075f089536097f3acd92e31d
milenaonmaui <milenakari@yahoo.com>
2019-09-11 14:13:55 -0700

    refactor repositiries#index

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,10 +1,19 @@
 class RepositoriesController < ApplicationController
   
   def index
-    resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    resp = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
     @repos = JSON.parse(resp.body)
-
-  end
+  
+     user_resp = Faraday.get "https://api.github.com/user" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
+    user_json = JSON.parse(user_resp.body)
+    session[:username] = user_json["login"]
+   end
 
   def create
     resp = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
