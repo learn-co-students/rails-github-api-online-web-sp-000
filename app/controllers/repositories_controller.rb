7eb7307aacc22d8ae8c7fcf76f8d7e271beff732
Andrew Davisson <akdavisson4@gmail.com>
2019-09-27 11:54:46 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,19 @@
 class RepositoriesController < ApplicationController
   
   def index
+    @user = get_user
+    @resp = Faraday.get "https://api.github.com/users/#{@user}/repos" do |req|
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+    end
+
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repositories = body
+    else
+      @error = "Failed"
+    end
+    render 'index'
   end
 
 end
